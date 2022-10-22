342df56a1dc9d99a72c6a356bb40c8408a854b0e
jacobjavacoding <87937458+jacobjavacoding@users.noreply.github.com>
2022-10-22 12:35:37 +0530

    Add files via upload

--- src/main/java/rabbitmq/rabbitmq/RabbitMQConfig.java
+++ src/main/java/rabbitmq/rabbitmq/RabbitMQConfig.java
@@ -1,48 +1,41 @@
 package rabbitmq.rabbitmq;
 
-
-
-
 import javax.annotation.PostConstruct;
 
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 @Configuration
 public class RabbitMQConfig {
-	
+
 	@Autowired
 	private AmqpAdmin amqpAdmin;
 
 	@Value("${rabbitmq.queue.name}")
-	private String queue;
+	private String queueName;
 
 	@Value("${rabbitmq.exchange.name}")
-	private String exchange;
+	private String exchangeName;
 
 	@Value("${rabbitmq.routing.key}")
 	private String routingKey;
 
-	
-	
 	@PostConstruct
-    public void createQueues() {
-        amqpAdmin.declareQueue(new Queue(queue, true));
-        amqpAdmin.declareExchange(new TopicExchange(exchange) );
-        amqpAdmin.declareBinding(BindingBuilder.bind(new Queue(queue, true)).to(new TopicExchange(exchange)).with(routingKey));
-        
-    }
+	public void createQueues() {
+		TopicExchange exchange = new TopicExchange(exchangeName);
+		Queue queue1 = new Queue(queueName);
+		amqpAdmin.declareExchange(exchange);
+		amqpAdmin.declareQueue(queue1);
+		Binding binding = BindingBuilder.bind(queue1).to(exchange).with(routingKey);
+		amqpAdmin.declareBinding(binding);
 
+	}
 
-	
-	
 }
